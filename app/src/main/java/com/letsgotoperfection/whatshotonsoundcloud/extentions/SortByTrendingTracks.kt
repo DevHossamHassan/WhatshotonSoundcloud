2c1566819b0defa5403332d4b520d0cf5124286f
DevHossamHassan <hossamhassancs15@gmail.com>
2018-01-29 16:28:26 +0200

    Updating the ranking method to include the release date

--- app/src/main/java/com/letsgotoperfection/whatshotonsoundcloud/extentions/SortByTrendingTracks.kt
+++ app/src/main/java/com/letsgotoperfection/whatshotonsoundcloud/extentions/SortByTrendingTracks.kt
@@ -7,7 +7,7 @@ import com.letsgotoperfection.whatshotonsoundcloud.models.Track
  */
 fun MutableList<Track>.sortByTrendingTracks(): MutableList<Track> {
     return (this.distinctBy({ it.id }).sortedWith(
-            compareBy({ it.playback_count }, { it.favoritings_count }, { it.reposts_count })))
+            compareBy({ it.playback_count }, { it.favoritings_count }, { it.reposts_count }, { it.created_at })))
             .reversed()
             .toMutableList()
 }
